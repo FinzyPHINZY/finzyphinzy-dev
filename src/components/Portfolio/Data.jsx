c1409c2a35ac9810833945f201932c42eb8e303c
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-02-25 14:15:38 +0100

    feat: updated project

--- src/components/Portfolio/Data.jsx
+++ src/components/Portfolio/Data.jsx
@@ -1,10 +1,13 @@
-import Work2 from '../../assets/carepulse.png';
-import Work4 from '../../assets/forever.png';
-import Work7 from '../../assets/finzystore.png';
-import Work9 from '../../assets/hilink.png';
 import Work1 from '../../assets/largescreen-homepage-snapcase.png';
-import Work3 from '../../assets/positivus.png';
-import Work10 from '../../assets/brickandbeam.png';
+import Work2 from '../../assets/forever.png';
+import Work3 from '../../assets/layersdesign.svg';
+import Work4 from '../../assets/hilink.png';
+import Work5 from '../../assets/nexxsass.svg';
+import Work6 from '../../assets/reqaps.svg';
+import Work9 from '../../assets/positivus.png';
+import Work10 from '../../assets/carepulse.png';
+import Work11 from '../../assets/finzystore.png';
+import API from '../../assets/api.svg';
 
 export const projectsData = [
   {
@@ -12,44 +15,85 @@ export const projectsData = [
     image: Work1,
     title: 'Snapcase',
     description:
-      'SnapCase is a web application that allows users to import a picture an order a custom phone case with the picture as the case cover.',
+      'SnapCase is a web application that allows users to import a picture and order a custom phone case with the picture as the case cover.',
     link: 'https://snapcase-one.vercel.app',
     repo: 'https://github.com/FinzyPHINZY/snapcase',
     category: 'applications',
   },
   {
-    id: 4,
-    image: Work4,
+    id: 2,
+    image: Work2,
     title: 'Forever',
     description:
-      'Forever is a sleek and responsive e-commerce platform designed for seamless shopping featuring intuitive navigation, a dynamic product catalog, a cart system with secure checkout, and user authentication.',
+      'Forever is a responsive e-commerce platform designed for seamless shopping featuring multiple functionalities.',
     link: 'https://buyatforever.vercel.app/',
     repo: 'https://github.com/FinzyPHINZY/forever-frontend',
     category: 'applications',
   },
   {
-    id: 9,
-    image: Work9,
+    id: 3,
+    image: Work3,
+    title: 'layers',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius temporibus excepturi, deserunt rem repellendus nostrum.',
+    link: 'https://layersdesign.vercel.app/',
+    repo: 'https://github.com/FinzyPHINZY/layers',
+    category: 'websites',
+  },
+  {
+    id: 4,
+    image: Work4,
     title: 'HiLink.',
     description:
-      'Hilink is a hiking and climbing companion application that helps adventurers navigate and plan their journeys. It provides real-time information, offline maps, and augmented reality features to enhance the outdoor experience.',
+      'Hilink is a landing page for an hiking and climbing companion application that helps adventurers navigate and plan their journeys. ',
     link: 'https://hilink-travelcamp.vercel.app/',
     repo: 'https://github.com/FinzyPHINZY/hi-link',
     category: 'websites',
   },
   {
-    id: 10,
-    image: Work10,
-    title: 'Estate',
+    id: 5,
+    image: Work5,
+    title: 'nexxsass',
     description:
-      'Estate is a sleek and interactive frontend project built with React.js and Framer Motion for a real estate company showcasing available properties, highlights completed projects, and provides an intuitive user experience for potential buyers looking for their dream homes.',
-    link: 'https://estate-finzyphinzy.vercel.app/',
-    repo: 'https://github.com/FinzyPHINZY/estate',
+      'A modern, responsive landing page for Nexx - an open-source tool designed to help developers launch their ideas faster. ',
+    link: 'https://nexxsass.vercel.app/',
+    repo: 'https://github.com/FinzyPHINZY/nexxsass',
     category: 'websites',
   },
   {
-    id: 2,
-    image: Work2,
+    id: 6,
+    image: Work6,
+    title: 'reqaps',
+    description:
+      'A modern, responsive landing page for Nexx - an open-source tool designed to help developers launch their ideas faster. ',
+    link: 'https://reqaps.vercel.app',
+    repo: 'https://github.com/FinzyPHINZY/reqaps',
+    category: 'websites',
+  },
+  {
+    id: 9,
+    image: Work9,
+    title: 'Positivus',
+    description:
+      'Positivus is a sleek frontend for a digital marketing agency, showcasing SEO, PPC, and social media services with an engaging user experience.',
+    link: 'https://positivus-digital.vercel.app/',
+    repo: 'https://github.com/FinzyPHINZY/positivus',
+    category: 'websites',
+  },
+
+  {
+    id: 11,
+    image: Work11,
+    title: 'FinzyStore',
+    link: 'https://finzystore.vercel.app/',
+    repo: 'https://github.com/FinzyPHINZY/finzystore',
+    description:
+      'FinzyStore is an e-commerce web application. Features a responsive design, shopping cart functionality, and more.',
+    category: 'websites',
+  },
+  {
+    id: 10,
+    image: Work10,
     title: 'CarePulse',
     description:
       'A healthcare patient management application that allows patients to easily register, book, and manage their appointments with doctors',
@@ -57,25 +101,25 @@ export const projectsData = [
     repo: 'https://github.com/FinzyPHINZY/carepulse',
     category: 'applications',
   },
-
   {
-    id: 3,
-    image: Work3,
-    title: 'Positivus',
+    id: 19,
+    image: API,
+    title: 'forever',
     description:
-      'Positivus is a sleek React.js and Framer Motion-powered frontend for a digital marketing agency, showcasing SEO, PPC, and social media services with a smooth, engaging user experience.',
-    link: 'https://positivus-digital.vercel.app/',
-    repo: 'https://github.com/FinzyPHINZY/positivus',
-    category: 'websites',
+      'This is an e-commerce REST API that handles user authentication, product management with image uploads to Cloudinary, shopping cart functionality, and order processing with both cash-on-delivery and Stripe payment options. It provides all the essential endpoints needed for a full-featured online store.',
+    link: 'https://github.com/FinzyPHINZY/forever-backend',
+    repo: 'https://github.com/FinzyPHINZY/forever-backend',
+    category: 'APIS',
   },
-
   {
-    id: 7,
-    image: Work7,
-    title: 'FinzyStore',
-    link: 'https://finzystore.vercel.app/',
-    repo: 'https://github.com/FinzyPHINZY/finzystore',
-    category: 'websites',
+    id: 20,
+    image: API,
+    title: 'mernmunch',
+    description:
+      'This is a food delivery API that allows users to browse restaurants, place orders, and process payments. Restaurant owners can manage their menus, handle orders, and update delivery statuses. The system uses Auth0 for authentication, Stripe for payments, and Cloudinary for image storage, with MongoDB as the database.',
+    link: 'https://github.com/FinzyPHINZY/mernmunch/tree/main/backend',
+    repo: 'https://github.com/FinzyPHINZY/mernmunch/tree/main/backend',
+    category: 'APIS',
   },
 ];
 
@@ -89,5 +133,8 @@ export const projectsNav = [
   },
   {
     name: 'websites',
+  },
+  {
+    name: 'APIs',
   },
 ];
--- src/components/Portfolio/WorkItems.jsx
+++ src/components/Portfolio/WorkItems.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const WorkItems = ({ item }) => {
   return (
@@ -7,17 +7,17 @@ const WorkItems = ({ item }) => {
       <h3 className="work__title">{item.title}</h3>
       <p className="work__description">{item.description}</p>
       <div className="work__context">
-        <a href={item.link} className="work__button">
+        <a href={item.link} className="work__button" target="_blank">
           Live Demo
           <i className="bx bx-right-arrow-alt work__button-icon"></i>
         </a>
-        <a href={item.repo} className="work__button">
+        <a href={item.repo} className="work__button" target="_blank">
           Repo
           <i className="bx bx-right-arrow-alt work__button-icon"></i>
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WorkItems
+export default WorkItems;
